c870958494ff70424dd8c49a21ca52954c638de4
holidayfhd <1029323816@qq.com>
2021-10-18 23:31:37 +0800

    shallowEqual

--- src/pages/button/index.js
+++ src/pages/button/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 function Button(props) {
   const { color } = useSelector(
     (state) => ({ color: state.color }),
-    shallowEqual
+    shallowEqual //进行浅等式比较 而不是===
   );
   return (
     <div className="btnContainer">
