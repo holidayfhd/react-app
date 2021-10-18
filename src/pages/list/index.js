eb1e75866228d628e78bbe345f63773e48c06727
holidayfhd <1029323816@qq.com>
2021-10-18 23:42:19 +0800

    add List组件

---
+++ src/pages/list/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+function List(props) {
+  return <div>List</div>;
+}
+
+export default memo(List);
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,13 @@
 import React, { memo } from "react";
 import Color from "../color";
 import Button from "../button";
+import List from "../list";
 
 export default memo(function Main() {
   return (
     <div className="container">
       <Button />
+      <List />
       <Color />
     </div>
   );
