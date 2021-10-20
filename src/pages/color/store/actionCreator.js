c571c77f8329923195b363d075349c928e69ffd3
holidayfhd <1029323816@qq.com>
2021-10-20 11:34:31 +0800

    引入redux-action

--- src/pages/color/store/actionCreator.js
+++ src/pages/color/store/actionCreator.js
@@ -1,6 +1,8 @@
+import { createAction } from "redux-actions";
 import * as actionTypes from "./constants";
 
-export const changeBg = (color) => ({
-  type: actionTypes.CHANGE_BG,
-  color,
-});
+// export const changeBg = (color) => ({
+//   type: actionTypes.CHANGE_BG,
+//   color,
+// });
+export const changeBg = createAction(actionTypes.CHANGE_BG, (color) => color);
--- src/pages/color/store/reducer.js
+++ src/pages/color/store/reducer.js
@@ -1,11 +1,22 @@
+import { handleAction } from "redux-actions";
+import { changeBg } from "./actionCreator";
+import * as actionTypes from "./constants";
 /* eslint-disable import/no-anonymous-default-export */
 const defaultState = "red";
-export default (state = defaultState, action) => {
-  switch (action.type) {
-    case "color/CHANGE_BG":
-      console.log(state, action);
-      return action.color;
-    default:
-      return state;
-  }
-};
+// export default (state = defaultState, action) => {
+//   switch (action.type) {
+//     case "color/CHANGE_BG":
+//       console.log(state, action);
+//       return action.color;
+//     default:
+//       return state;
+//   }
+// };
+const colorReducer = handleAction(
+  actionTypes.CHANGE_BG,
+  (state, action) => {
+    return action.payload;
+  },
+  defaultState
+);
+export default colorReducer;
